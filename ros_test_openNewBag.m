3d0863ef0f446bd05386f032c058c69ffb28b485
andreasmarxer <marxera@ethz.ch>
2019-11-01 10:44:58 +0100

    written rosbag can be read again when using https://github.com/gavanderhoorn/rosbag_fixer previously git add ros_test_openNewBag.m !

--- ros_test_openNewBag.m
+++ ros_test_openNewBag.m
@@ -1,5 +1,16 @@
 clear; clc; close all;
 %%=========================================================================
-filename = '/home/andreas/2019-10-31-17-33-18.bag';
+filename = '/home/andreas/test_fix.bag';
 bag = rosbag(filename);
-% bagInfo = rosbag('info',filename)
+bagInfo = rosbag('info',filename);
+
+topic = '/depth_adapted/image';
+frame = 1;
+
+topic_bag = select(bag, 'Topic', topic);
+topic_frame_msg_cell = readMessages(topic_bag, frame); % take image message of frames of bag
+topic_frame_msg = topic_frame_msg_cell{1}; % extract from cell
+
+sec = topic_frame_msg.Header.Stamp.Sec;
+nsec = topic_frame_msg.Header.Stamp.Nsec;
+time_s = sec + nsec/10^9;
