888a7d688db2ed8a2de5a2f178cd79551e5683f5
andreasmarxer <marxera@ethz.ch>
2019-11-01 10:08:26 +0100

    correct order of image backtransformation to bag

--- g_PngImage2Bag.m
+++ g_PngImage2Bag.m
@@ -1,5 +1,5 @@
 %% params
-frame = 2;
+label = 2;
 rotation_back = true;
 print = false;
 save_img = false;
@@ -9,15 +9,22 @@ path = '/home/andreas/Documents/ASL_window_dataset/depth_images_adj/';
 filename = strcat(path, 'asl_window_', num2str(label), '_depth_adj.png');
 
 %%
+
 img = imread(filename); % PNG image: uint16, [mm]
+
+img = single(img); % backtransform to single
+
+zero_idx = img < 0.0001;
+img(zero_idx) = NaN; % backtransform zeros to NaN
+
 img = img/1000; % backtransform to [m]
-img(img == 0)=NaN; % backtransform zeros to NaN
 
 if rotation_back == true
     img = imrotate(img, 180);
 end
+        
+        
 
-img = single(img); % backtransform to single
 
 % NEED TO GENERATE MESSAGE BEFORE !!!!!!!!!!!!!!!!!!!!!!!
 writeImage(msg, img);
