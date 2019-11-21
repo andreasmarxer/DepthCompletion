6867ce22d5f012ca3893f6e30aa39fddb959695d
andreasmarxer <marxera@ethz.ch>
2019-11-21 18:43:09 +0100

    added written message for inspection, changed path

--- g_PngImage2Bag.m
+++ g_PngImage2Bag.m
@@ -2,29 +2,32 @@
 label = 2;
 rotation_back = true;
 print = false;
-save_img = false;
 %%
 
-path = '/home/andreas/Documents/ASL_window_dataset/depth_images_adj/';
-filename = strcat(path, 'asl_window_', num2str(label), '_depth_adj.png');
+path = '/home/andreas/Documents/DepthAdaptation/';
+filename = strcat(path, 'asl_window_depth_',  num2str(label), '_adj.png');
 
 %%
 
-img = imread(filename); % PNG image: uint16, [mm]
+img_png = imread(filename); % PNG image: uint16, [mm]
 
-img = single(img); % backtransform to single
+img_png = single(img_png); % backtransform to single
 
-zero_idx = img < 0.0001;
-img(zero_idx) = NaN; % backtransform zeros to NaN
+zero_idx = img_png < 0.0001;
+img_png(zero_idx) = NaN; % backtransform zeros to NaN
 
-img = img/1000; % backtransform to [m]
+img_png = img_png/1000; % backtransform to [m]
 
 if rotation_back == true
-    img = imrotate(img, 180);
+    img_png = imrotate(img_png, 180);
 end
-        
-        
+                
+msg_test = rosmessage('sensor_msgs/Image');
+msg_test.Encoding = '32FC1';
+msg_test.Step = 2560;
 
+writeImage(msg_test, img_png);
 
-% NEED TO GENERATE MESSAGE BEFORE !!!!!!!!!!!!!!!!!!!!!!!
-writeImage(msg, img);
+disp('-Written');
+msg_test
+data_write = msg_test.Data;
