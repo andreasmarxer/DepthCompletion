4002d2a330a1571126187afd2a10ea043f2269aa
andreasmarxer <marxera@ethz.ch>
2019-11-21 18:41:54 +0100

    changed path

--- g_bag2PngImage.m
+++ g_bag2PngImage.m
@@ -1,21 +1,24 @@
-clear; clc; close all;
+%clear; clc; close all;
 %%=========================================================================
-filename = '/home/andreas/Documents/ros_recording/2019-10-15-17-48-49.bag';
+filename = '/media/andreas/SanDiskSSD/Semester_Thesis/ros_recording/2019-10-15-17-48-49_original.bag';
 bag = rosbag(filename);
 
 %% params
-frame = 2;
+frame = 1;
 rotation = true;
 print = false;
-save_img = false;
+save_img = true;
+type = 'depth';
 %%
 
 topic = 'medium_resolution/depth_registered/image';
 topic_bag = select(bag, 'Topic', topic);
 
 topic_frame_msg_cell = readMessages(topic_bag, frame); % take image message of frames of bag
-topic_frame_msg = topic_frame_msg_cell{1}; % extract from cell
-img = readImage(topic_frame_msg);   % 480x640 single, with NaNs, [m] % !!!!!!!!!
+disp('-Original');
+topic_frame_msg = topic_frame_msg_cell{1} % extract from cell
+img_original = readImage(topic_frame_msg);   % 480x640 single (32bit), with NaNs, [m] % !!!!!!!!!
+img = img_original;
 
 if rotation == true
     img = imrotate(img, 180);
@@ -27,13 +30,15 @@ if print == true
 end
 
 if save_img == true
-        filename = strcat('asl_window_', num2str(type), '_', num2str(label), '.png');
+        filename = strcat('asl_window_', num2str(type), '_', num2str(frame), '_adj.png');
         img(isnan(img))=0;
-        convert from m to mm
+        % convert from m to mm
         img = img * 1000;  
-        convert to uint16
-        img_uint16 = uint16(img);
-        imwrite(img_uint16, filename, 'fmt', 'png');
+        % convert to uint16
+        img = uint16(img);
+        imwrite(img, filename, 'fmt', 'png');
 end 
+
+data_original = topic_frame_msg.Data;
 
 % PNG image: uint16, [mm]
