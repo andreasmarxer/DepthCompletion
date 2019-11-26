6fca57ce78ef70b6d3819807b8707da0ec796e77
andreasmarxer <marxera@ethz.ch>
2019-11-26 20:58:43 +0100

    added proper description of function

--- plot_bb.m
+++ plot_bb.m
@@ -1,6 +1,12 @@
 function [x1, y1, x2, y2, x3, y3, x4, y4 ] = plot_bb(left_x_vec, top_y_vec, width_vec, height_vec, img)
-
+% plots predicted bounding box corner points
+% input:    bounding boxes in YOLOv3 prediction format
+%           vector length is number of predictions in image
+%           left_x_vec, top_y_vec: coordinates of top left point in [px]
+%           width_vec, height_vec: width and height of bb in [px]
+% output:   none, plots bounding box corners in figure
     for window = 1:size(left_x_vec)
+        % convert YOLOv3 format to corner point
         [x1, y1, x2, y2, x3, y3, x4, y4 ] = convertToBBCoords(left_x_vec(window), top_y_vec(window), width_vec(window), height_vec(window), img);
 
         hold on
