5195453bc243e2cfe7b8b6ab74f3c88e202cd70b
andreasmarxer <marxera@ethz.ch>
2019-12-15 14:45:31 +0100

    added proper comments and cleaned up code

--- depthCompletion_gravityAlignedLines.m
+++ depthCompletion_gravityAlignedLines.m
@@ -1,23 +1,21 @@
+% script to complete the depth image based on bounding box predictions
+
 clear; clc; close all;
-%%=========================================================================
-%% parameters
 
-%%%%%%%%%%%%%%%%%%%
-ploting = true;
-rotate_back = true; % rotate the processed image before saving png
-% images need to have corrected upright  view due to prediction bouding box format
-debug = false;
-save = true;
-save_figures = false;
-label = 130;
-%%%%%%%%%%%%%%%%%%%
+%% parameters =============================================================
+% settings
+rotate_back = true;     % rotate the processed image back before saving png
 
+ploting = true;         % plot for every label the rgb, depth and completed depth
+debug = false;          % outputs and more plots
+save = true;            %
+save_figures = false;   % save the figure with rgb, depth and completed depth
+label = 353;            % label to do depth completion, use 0 for loop with all
+% -------------------------------------------------------------------------
+% fixed parameters - do not change !
 confidence_th = 95; % [%] above which prediction confidence consider window
 dist_th = 50;
-% not used in actual approach but still needed as input in functions
-bb_width = 7;
-inside = false;
-
+%%=========================================================================
                 
 if label == 0
     label_array = 2:1:496; 
@@ -31,12 +29,15 @@ for label = label_array
     tic;
 
     %% fix dependencies
-    dataset = 'original5'; % loads the correct pose file and saving imgs
+    % define paths of images
+    dataset = 'original5'; % used for loading the correct pose file
     path = 'dataset/original5/';
+    rgb_filename = strcat(path, 'rgb_images/' ,'asl_window_', 'rgb_', num2str(label), '.jpg');    
     depth_filename = strcat(path, 'depth_images_mm/' ,'asl_window_', 'depth_', num2str(label), '.png');
     depth_filename_median = strcat(path, 'depth_images_median5/','asl_window_', 'depth_median5_', num2str(label), '.png');
+    
+    % load predicted bounding boxes
     bb_filename = strcat(path, 'rgb_images_predictions/' ,'asl_window_','rgb_', num2str(label), '.txt');
-    rgb_filename = strcat(path, 'rgb_images/' ,'asl_window_', 'rgb_', num2str(label), '.jpg');
     [class, confidence, left_x_vec, top_y_vec, width_vec, height_vec] = importBoundingBoxes(bb_filename);
 
     %% only consider windows with a confidence higher than the threshold
@@ -51,14 +52,13 @@ for label = label_array
     %% rgb image
     rgb_img = imread(rgb_filename);
 
-    %% median filtered (kernel 5x5) depth image
+    %% median filtered (kernel 5x5) depth image (REPORT 3.2.1)
+    % directly load filtered images generated with kernelMedian.m
     depth_img = imread(depth_filename_median); % meters
 
-    %% adjust the depth image
-    % initialization
+    %% initialize completed depth image
     depth_img_adj = depth_img;  % finally contains depth measurment and fitted planes
-    depth_img_corr = uint16(zeros(size(depth_img))); % only contains fitted planes
-    plane_pts = uint16([]); % points chosen to fit plane for all predictions
+    depth_img_corr = uint16(zeros(size(depth_img)));  % only contains fitted planes !
 
     %% VISUALISATION
     if ploting == true
@@ -94,30 +94,31 @@ for label = label_array
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% for every prediction in this image
     for window = 1:size(left_x_vec)
-        % get the predicted bounding box coordinates
-        % !!! 1: top left, 2: top right, 3: btm right, 4: btm left !!!
+        % convert the predicted bounding box coordinates to 4 corner points
+        % convention: 1: top left, 2: top right, 3: btm right, 4: btm left
         [x1, y1, x2, y2, x3, y3, x4, y4 ] = ...
             convertToBBCoords(left_x_vec(window), top_y_vec(window), ...
             width_vec(window), height_vec(window), depth_img);
 
-        %% edge detection
-        % 1. returns line segments found in bounding box with Canny and Hough Transform
-        lines_depth = getLinesInBB(depth_img, x1, x2, y1, y3, bb_width, debug);  
-        lines_rgb = getLinesInBB(rgb_img, x1, x2, y1, y3, bb_width, debug);
-
-        % 2. calculate angle to gravity vector and delete other lines
+        %% edge detection with hough and canny (REPORT 3.2.2)
+        % returns line segments found in bounding box with Canny and Hough Transform
+        lines_depth = getLinesInBB(depth_img, x1, x2, y1, y3);  
+        lines_rgb = getLinesInBB(rgb_img, x1, x2, y1, y3);
+        
+        %% edge classification based on gravity vector (REPORT 3.2.3)
+        % calculate angle to gravity vector and delete other lines
         [lines_depth, ~] = convert2cameraAlignedGravity(lines_depth, x1, y1, depth_img, label, dataset, debug);
         [lines_rgb, gravity_cam] = convert2cameraAlignedGravity(lines_rgb, x1, y1, depth_img, label, dataset, debug);
 
-        % 3. delete all lines where we don't have depth measurments
+        % delete all lines where we don't have depth measurments
         lines_depth = deleteLinesWoDepth(lines_depth, debug);
         lines_rgb = deleteLinesWoDepth(lines_rgb, debug);
 
-        % 4. delete all lines which dont are horizontal / vertical
+        % delete all lines which dont are horizontal / vertical
         lines_depth = deleteNonHorizontalOrVerticalLines(lines_depth, debug);
         lines_rgb = deleteNonHorizontalOrVerticalLines(lines_rgb, debug);
 
-        % 5. take lines from rgb and color together
+        % take lines from rgb and color together
         if size(lines_depth,2)~=0 && size(lines_rgb,2)~=0
             lines = [lines_depth, lines_rgb];
         elseif size(lines_depth,2)~=0 && size(lines_rgb,2)==0
@@ -129,7 +130,7 @@ for label = label_array
             break;
         end
 
-        %% make RANSAC in camera frame
+        %% candidate plane fitting with RANSAC (REPORT 3.2.4)
         % RANSAC initialization
         clear best;
         best.first.random2 = zeros(1,2);
@@ -166,20 +167,21 @@ for label = label_array
             % initialize not unique!
             random2 = ones(1,2);
 
-            % initialize (crit=1/true means we do pick again)
+            % initialize (crit=1 means we do pick again)
             must_ransac_crit = 1;
             opt_ransac_crit = 1;
-
+            
+            % counter for iterations
             while_it = 1;
             n_tries_before_skip_ransac_it = 1000;
 
-            % while loop ---------------------------------------------
+            % while loop --------------------------------------------------
             while must_ransac_crit || opt_ransac_crit
 
                 % pick two random lines
                 random2 = randi([1 size(lines,2)],1,2);
 
-                % update the criterions that we have 3 different points
+                % update the criterions that we have 2 different lines
                 must_ransac_crit = size(unique(random2),2) ~= 2;
 
                 % check if the selected lines are perpendicular
@@ -187,7 +189,7 @@ for label = label_array
                 % remember that we only have ~90° or ~0° angles +- 5°
                 opt_ransac_crit = angle_diff < 10; 
 
-                % if did not find 3 points that match the criterions in 1000 iterations
+                % skip iteration if not found lines fulfilling criterions
                 if while_it > n_tries_before_skip_ransac_it
                     ransac_it_skipped = ransac_it_skipped + 1;
                     break % out of while loop
@@ -202,7 +204,7 @@ for label = label_array
                 continue % go to next ransac_it for loop
             end
 
-            %% fit plane from 2 planes
+            %% fit plane from 2 lines
             curr_lines = lines(random2);
 
             % first selected line
@@ -213,14 +215,13 @@ for label = label_array
             points_x_c_2 = [curr_lines(2).point1_cam(1); curr_lines(2).point2_cam(1)];
             points_y_c_2 = [curr_lines(2).point1_cam(2); curr_lines(2).point2_cam(2)];
             points_z_c_2 = [curr_lines(2).point1_cam(3); curr_lines(2).point2_cam(3)];
-            % order points
+            % group all coordinates
             points_x = [points_x_c_1; points_x_c_2];
             points_y = [points_y_c_1; points_y_c_2];
             points_z = [points_z_c_1; points_z_c_2];
 
-            % fit plane through 2 lines
+            % fit plane with least-squares linear regression through 2 lines
             param = pts2plane([points_x, points_y, points_z]);
-            %param = [points_x, points_y, ones(size(points_x,1),1)] \ [points_z];
 
             % calculate distances
             dist_1 = dist2plane(points1_cam, param);
@@ -231,7 +232,7 @@ for label = label_array
             inlier_array = (dist_1 < dist_th) & (dist_2 < dist_th);
             inliers = nnz(inlier_array);
 
-            % save new best parameters of ransac
+            %% check if this 2 lines already are in the best 3 candidates
             random2_variants = [random2 ; random2(2), random2(1)];
             random2_array = repmat(random2_variants,3,1);
 
@@ -243,22 +244,27 @@ for label = label_array
             check = check_if_already_in(:,1) & check_if_already_in(:,2);
 
             if nnz(check) > 0
-                %disp('already in best candidates, skipt this iteration')
+                % if these exact lines already are inside the best 3
+                % candidates skip this iteration
                 continue;
             end
-            % compare the current plane with the best 3 and update
+            %% compare the current plane with the best 3 and update
             best = compareWith3BestCandidates(random2, inliers, inlier_array, param, dist, best);
 
         end
         %% end of ransac ==============================================
 
-        if best.first.random2 == [0,0];
+        if best.first.random2 == [0,0]
+            % no plane found for this window
             disp(strcat('Skipped window : ', num2str(window), ' no plane fitted'));
             continue;
         end
-        %% project back to depth image and select a line for top/bot etc.
+        
+        %% plane selection (REPORT 3.2.5)
+        % project back to depth image and select a line for top/bot etc.
         spanned_area_in_px = zeros(4,1);
-
+        
+        % calculate area spanned for each of the candidates
         for plane = 1:4
             if plane == 1
                 selection = best.first.inlier_array;
@@ -270,6 +276,7 @@ for label = label_array
                 selection = best.third.inlier_array;
                 best_param = best.third.param;
             elseif plane == 4
+                % take candidate which spannes largest area
                 selection_plane = find(max(spanned_area_in_px)==spanned_area_in_px);
 
                 if selection_plane == 1
@@ -283,7 +290,8 @@ for label = label_array
                     best_param = best.third.param; 
                 end                        
             end
-
+            
+            % select plane which spannes largest area in image
             sel_lines = lines(selection);
 
             pause(0.2);
@@ -293,18 +301,19 @@ for label = label_array
             figure_title = strcat('all selected lines from plane :', num2str(plane));
             plot_lines(figure_id, figure_title, x1, x2, y1, y3, sel_lines, debug, depth_img)
             pause(1);
-
-            % top and bot start and end point in bb coordinates !!!
-            [xy_bb_top, xy_bb_bot] = getTopBotLinesInBBwGravity_2(...
+            
+            % top and bot start and end point
+            [xy_bb_top, xy_bb_bot] = getTopBotLinesInBBwGravity(...
                 sel_lines, x1, x2, y1, y3, depth_img, debug); %figure(9)
 
-            % left and right start and end point in bb coordinates !!!
-            [xy_bb_left, xy_bb_right] = getLeftRightLinesInBBwGravity_2(...
+            % left and right start and end point
+            [xy_bb_left, xy_bb_right] = getLeftRightLinesInBBwGravity(...
                 sel_lines, x1, x2, y1, y3, depth_img, debug); %figure(10)
 
             x_all = [xy_bb_top(:,1); xy_bb_bot(:,1); xy_bb_left(:,1); xy_bb_right(:,1)];
             y_all = [xy_bb_top(:,2); xy_bb_bot(:,2); xy_bb_left(:,2); xy_bb_right(:,2)];
-
+            
+            % calculate the spanned area in the image
             spanned_area_in_px(plane) = (max(x_all)-min(x_all))*(max(y_all)-min(y_all));
         end
 
@@ -312,11 +321,12 @@ for label = label_array
         % convert lines to function of other coordinate 
         % limiting the bounding box
         [y_top, y_bot, x_left, x_right] = line2functionInBB(...
-            x1, x2, y1, y3, xy_bb_top, xy_bb_bot, xy_bb_left, xy_bb_right,  bb_width, inside);
+            x1, x2, y1, y3, xy_bb_top, xy_bb_bot, xy_bb_left, xy_bb_right);
 
         % camera intrinsic with shifted principal point due to rotated image
         load('data/K_normalView.mat'); % loaded as K_normalView
-
+        
+        %% correcting the depth image (REPORT 3.2.6)
         % adjust depth image for each pixel inside bounding box
         for x = x1:1:x2
             for y = y1:1:y3
@@ -326,12 +336,11 @@ for label = label_array
                     % convert x,y [px] to normalized image coordinates x,y [m] (z=1m)
                     [X_norm, Y_norm] = pixelNormalView2normImgCoordinates(x, y, K_normalView);
 
-                    % calculate new Z value
-                    % n*[x_n; y_n; 1] = [X_c; Y_c; Z_c] 
-                    % where X_c, Y_c, Z_c is element of the plane 
+                    % calculate new Z value from normalized camera coords
+                    % Z = a*Z*X_norm + b*Z*Y_norm + d -> Z = -d/(aX_norm + bY_norm -1)
                     new_value = -best_param(3) / (best_param(1)*X_norm + best_param(2)*Y_norm -1);
 
-                    % if we have a crappy plane ignore it
+                    % if no plane found
                     if new_value < 1
                         continue;
                     % use the new value for every pixel if no depth is available
@@ -397,5 +406,4 @@ for label = label_array
 
     toc;
 
-    
 end % end of label for loop
