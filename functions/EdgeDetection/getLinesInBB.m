25be91d423d6d02bf6f2a335aac35d7d0b7956e0
andreasmarxer <marxera@ethz.ch>
2019-12-15 11:20:25 +0100

    delete old commented stuff and comment function for fill_gap th

--- functions/EdgeDetection/getLinesInBB.m
+++ functions/EdgeDetection/getLinesInBB.m
@@ -1,14 +1,10 @@
-function lines = getLines(depth_img_woraster, x1, x2, y1, y3, bb_width, debug)
+function lines = getLinesInBB(depth_img_woraster, x1, x2, y1, y3)
 % returns line segments found in bounding box with Canny and Hough Transform
 % the frame has a bend if the edge goes out of the bb area because we want
 % the frame always to stays inside the bounding box
 %
 % input:    depth_img_woraster: depth image in uint16 format
 %           x1, x2, y1, y3: bounding box coordinates
-%           bb_width:   width of frame along detected edges to take
-%                       measurements for RANSAC (previously used bounding
-%                       box and not detected edges, there the name comes from)
-%           debug:      boolean, if true prints detected edges etc.
 %
 % output:   lines:      struct array with fields: point1, point2, theta, rho
 
@@ -21,21 +17,21 @@ BW= edge(depth_img_woraster_BB, 'Canny');
 
 % 1. make hough transform: rho = x*cos(theta) + y*sin(theta)
 [Hough, Theta, Rho] = hough(BW, 'RhoResolution', 1, 'Theta', -90:89);
-%[Hough2, Theta2, Rho2] = hough(BW, 'RhoResolution', 1, 'Theta', 45:89);
 
-% 2. visualize peaks of hough transform
+% 2. take peaks of hough transform
 num_peaks = 50;
 Peaks  = houghpeaks(Hough, num_peaks, 'threshold', ceil(0.2*max(Hough(:)))); % peak
-%Peaks2  = houghpeaks(Hough2, num_peaks, 'threshold', ceil(0.5*max(Hough2(:)))); % peak
 
-% 3. find lines
-%fill_gap_th = round(1/20*(x2-x1)+5);
-%fill_gap_th = 5;
-
+% 3. set parameters for finding lines
+% parameters a,b, such that the fill_gap_th is:
+% 10 for a bounding box width of 100px
+%  6 for a bounding box width of 10px
 a = 0.04;
 b = 6;
 fill_gap_th = round(a*(x2-x1)+b); 
 min_lenght_th = round(1/4*(x2-x1));
+
+% 4. find lines
 lines = houghlines(BW, Theta, Rho, Peaks, 'FillGap', fill_gap_th, 'MinLength', min_lenght_th);
 
 end
